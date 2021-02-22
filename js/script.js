c643bfa9d4410caba90d86782b42d5cdae153ed0
Brian Beeler <brianlbeeler@gmail.com>
2021-02-22 15:05:03 -0600

    Ran through checklist of expectations.

--- js/script.js
+++ js/script.js
@@ -8,49 +8,104 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array - these are quotes that can be randomly selected for display
 ***/
 
 let quotes = [
   {
     quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
-    source: "-Benjamin Franklin",
+    source: "Benjamin Franklin",
     year: "~1776"
   }, {
     quote: "Whoever is happy will make others happy too.",
-    source: "-Anne Frank",
-    citation: "The Diary of a Young Girl"
+    source: "Anne Frank",
+    citation: '"The Diary of a Young Girl"'
   }, {
     quote: "In the end, it's not the years in your life that count. It's the life in your years.",
-    source: "-Abe Lincoln"
+    source: "Abe Lincoln",
+    personalFavorite: true
   }, {
     quote: "Only a life lived for others is a life worthwhile.",
-    source: "-Albert Einstein"
+    source: "Albert Einstein",
+    personalFavorite: true
   }, {
     quote: "Asante sana squash banana",
-    source: "-Rafiki",
+    source: "Rafiki",
     year: 1994,
-    citation: "The Lion King"
+    citation: '"The Lion King"'
   }
 ]
 
-console.log("Quotes:","\n", quotes) 
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function - returns a random quote from the quotes array
+ *  author: Brian Beeler
 ***/
 
-
+let getRandomQuote = () => {
+  let index = Math.floor( Math.random() * quotes.length);
+  return quotes[index];
+}
 
 /***
- * `printQuote` function
+ * `printQuote` function - gets a random quote, assembles and displays it in html
+ * auther: Brian Beeler
 ***/
 
+let printQuote = () => {
+  quote = getRandomQuote(quotes);
 
+  // Add quote and source to the html tag
+  html = `<p class="quote">${quote.quote}</p>`
+  html +=`<p class="source">${quote.source}`
+  
+  // Add optional properties: citation, year, personal favorite
+  if (quote.citation) {
+    html += `<span class="year">${quote.citation}</span>`
+  }
+  
+  if (quote.year) {
+    html += `<span class="year">${quote.year}</span>`
+  }
+
+  if (quote.personalFavorite) {
+    html += `<span class="pf"> (Personal Favorite)</p>`
+  }
+
+  // Closing tag
+  html+="</p>"
+
+  // Print finalized html template
+  console.log(html)
+
+  // Change the background color, then replace the quote html
+  random_bg_color();
+  document.getElementById('quote-box').innerHTML = html
+
+}
+
+/**
+ * `random_bg_color` - chooses random rgb color, and changes the html background to it
+ *  not original, source: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
+ */
+function random_bg_color() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  console.log(bgColor);
+  document.body.style.background = bgColor;
+}
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+/**
+ *  Automatically rotate quotes, every 5 seconds
+ */
+
+ setInterval(printQuote, 5000)
